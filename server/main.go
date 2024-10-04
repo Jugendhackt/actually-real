69d78333a496db2f2b526d9842797b753f7f3f7d
confusedalex <alex@confusedalex.dev>
2024-10-04 15:35:50 +0200

    prototype

--- server/main.go
+++ server/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"time"
 
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID      uint `gorm:"primaryKey,autoIncrement"`
+	Name    string
+	Friends []User `gorm:"many2many:friends"`
+	Images  []Image
+}
+
+type Image struct {
+	ID      uint `gorm:"primaryKey,autoIncrement"`
+	Path    string
+	UserID  uint
+	Created time.Time
+}
+
+func main() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&User{})
+	db.AutoMigrate(&Image{})
 }
